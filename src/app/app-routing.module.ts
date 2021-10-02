fe16462618ced79e202a38793999419695921477
Ratthaphon Chitchak <ratthaphon.c@g-able.com>
2021-10-02 18:02:54 +0700

    create component admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'authoriezd/admin/admin',
+    redirectTo: 'folder/folder',
     pathMatch: 'full'
   },
   {
