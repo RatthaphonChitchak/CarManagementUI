8a2fc420c506b60196524879374cab2279522ebf
Ratthaphon Chitchak <ratthaphon.c@g-able.com>
2021-10-02 17:50:17 +0700

    Create Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,20 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Index',
+    redirectTo: 'authoriezd/admin/admin',
     pathMatch: 'full'
   },
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'client',
+    loadChildren: () => import('./Client/client.module').then( m => m.ClientModule)
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then( m => m.AdminModule)
   }
 ];
 
